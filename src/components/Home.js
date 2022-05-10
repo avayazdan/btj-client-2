95dbc8d2a18c40238af682ab380589e1540aa38d
Ava Yazdany <avayaz1337@gmail.com>
2022-05-10 23:43:42 +0200

    semantic updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,9 @@
 import React from 'react'
 // import { Link } from 'react-router-dom'
+import "../App.css"
+
+
+<link rel="stylesheet" href="App.css"></link>
 
 function Home() {
   return (
@@ -10,6 +14,25 @@ function Home() {
         <h1>Hot this month</h1>
       </div>
     </div>
+    <div class="square-container">
+  <div class="row">
+    <div class="square">projecting politics: the grapes of wrath</div>
+    <div class="square">goodnight britain: an essay by Tom Doubtfire</div>
+    <div class="square">on the importance of Seneca</div>
+    <div class="square">Sitting Bull's fight for indigenous land</div>
+    <div class="square">punk planet: issue 277</div>
+    <div class="square">a look through Rumi's legacy</div>
+  </div>
+  </div>
+    <section>
+      </section>
+      <h2>
+        "Man's capacity for justice makes democracy possible, <br></br>
+        but man's inclination to injustice makes democracy necessary"
+      </h2>
+      <div class='home-photo-container'>
+
+      </div>
     </>
   )
 }
