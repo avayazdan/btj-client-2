fd3da514dec52772f1d13e1a5f7b11866ce53769
Ava Yazdany <avayaz1337@gmail.com>
2022-05-11 23:48:02 +0200

    added css row

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,33 +8,33 @@ import "../App.css"
 function Home() {
   return (
     <>
-    <div className='starting-container'>
-      <div className='heading-banner'></div>
-      <div className='hot'>
-        <h1>Hot this month</h1>
+      <div className='starting-container'>
+        <div className='heading-banner'></div>
+        <div className='hot'>
+          <h1>Hot this month</h1>
+        </div>
       </div>
-    </div>
-    <div class="square-container">
-  <div class="row">
-    <div class="square">projecting politics: the grapes of wrath</div>
-    <div class="square">goodnight britain: an essay by Tom Doubtfire</div>
-    <div class="square">on the importance of Seneca</div>
-    <div class="square">Sitting Bull's fight for indigenous land</div>
-    <div class="square">punk planet: issue 277</div>
-    <div class="square">a look through Rumi's legacy</div>
-  </div>
-  </div>
-    <section>
-      </section>
       <h2>
         "Man's capacity for justice makes democracy possible, <br></br>
         but man's inclination to injustice makes democracy necessary"
       </h2>
-      <div class='home-photo-container'>
+      <div className="all-squares">
+          <div className="row">
+            <div className="square-1">projecting politics: the grapes of wrath</div>
+            <div className="square-2">goodnight great britain: an essay by Tom Doubtfire</div>
+            <div className="square-3">on the importance of Seneca</div>
+              <div className="square-4">Sitting Bull's fight for indigenous land</div>
+              <div className="square-5"></div>
+              <div className="square-6">a look through Rumi's legacy</div>
+            </div>
+          </div>
+        <section>
+        </section>
+        {/* <div class='home-photo-container'>
 
-      </div>
-    </>
-  )
+        </div> */}
+      </>
+      )
 }
 
-export default Home
+      export default Home
