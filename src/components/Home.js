9965dfbdc1efeb408aab2094e27413f6463f7ba2
Ava Yazdany <avayaz1337@gmail.com>
2022-05-13 20:04:36 +0200

    homepage css updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import "../App.css"
 import Homepagegif from "../components/medievalhome.jpg"
 import { Link } from 'react-router-dom'
 import axios from "axios"
+import Backdrop from "../components/logo2.png"
 
 <link rel="stylesheet" href="App.css"></link>
 
@@ -43,6 +44,7 @@ function Home() {
         "Man's capacity for justice makes democracy possible, <br></br>
         but man's inclination to injustice makes democracy necessary"
       </h2>
+      <img className="backdrop" src={Backdrop} alt="plague-doctor-backdrop"></img>
       <div className='hot'>
           <h1>Hot this month</h1>
         </div>
@@ -50,13 +52,13 @@ function Home() {
         <img className="gif" src={Homepagegif} alt="BTJ Logo"></img>
         </div>
         <div className="all-squares">
-      {submissions.map(x => <div className="row"> 
-      <Link key={x.uniqueId}to={'/submissions/19/'} id="1" className="square-1"> projecting politics: the grapes of wrath</Link>
-          <Link to={'/submissions/17/'} className="square-2">goodnight great britain: an essay by Tom Doubtfire</Link>
-          <Link to={'/submissions/20/'} className="square-3">on the importance of Seneca</Link>
-          <Link to={'/submissions/23/'} className="square-4">Sitting Bull's fight for indigenous land</Link>
+      {submissions.map(x => <div className="row">
+      <Link key={x.uniqueId}to={'/submissions/19/'} id="1" className="square-1"> <span className='highlight'>projecting politics: the grapes of wrath </span></Link>
+          <Link to={'/submissions/17/'} className="square-2"><span className='highlight'>goodnight great britain: an essay by Tom Doubtfire</span></Link>
+          <Link to={'/submissions/20/'} className="square-3"><span className='highlight'>on the importance of Seneca</span></Link>
+          <Link to={'/submissions/23/'} className="square-4"><span className='highlight'>Sitting Bull's fight for indigenous land</span></Link>
           <Link to={'/submissions/16/'} className="square-5"></Link>
-          <Link to={'/submissions/21/'} className="square-6">a look through Rumi's legacy</Link>
+          <Link to={'/submissions/21/'} className="square-6"><span className='highlight'>a look through Rumi's legacy</span></Link>
       </div>)}
     </div>
       </>
